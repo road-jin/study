583e7ec97459a8cfedabb111650d009dd83dc92d
road-jin <rjswmtk@gmail.com>
2024-01-22 08:34:03 +0900

    feat: 인프런 코테 1문제 학습
    큰 수 출력하기

---
+++ condingtest/src/main/java/string/Inflearn_2_1.java
@@ -0,0 +1,45 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Inflearn_2_1 {
+
+	public static void main(String[] args) throws IOException {
+		Inflearn_2_1 inflearn_2_1 = new Inflearn_2_1();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] numbers = getNumbers(br);
+
+		List<Integer> nextBiggerNumber = inflearn_2_1.getNextBiggerNumber(n, numbers);
+		print(nextBiggerNumber);
+	}
+
+	private static int[] getNumbers(BufferedReader br) throws IOException {
+		return Arrays.stream(br.readLine()
+				.split(" "))
+			.mapToInt(Integer::parseInt)
+			.toArray();
+	}
+
+	private static void print(List<Integer> nextBiggerNumber) {
+		nextBiggerNumber.forEach(n -> System.out.printf("%d ", n));
+	}
+
+	private List<Integer> getNextBiggerNumber(int n, int[] numbers) {
+		List<Integer> nextBiggerNumbers = new ArrayList<>();
+		nextBiggerNumbers.add(numbers[0]);
+
+		for (int i = 1; i < n; i++) {
+			if (numbers[i] > numbers[i - 1]) {
+				nextBiggerNumbers.add(numbers[i]);
+			}
+		}
+
+		return nextBiggerNumbers;
+	}
+}
