fe9034393cb9434944dcdd3a4b8d4c96cab04451
road-jin <rjswmtk@gmail.com>
2024-01-25 08:45:52 +0900

    feat: 인프런 코테 1문제 학습
    보이는 학생

---
+++ condingtest/src/main/java/string/Inflearn_2_2.java
@@ -0,0 +1,40 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Inflearn_2_2 {
+
+	public static void main(String[] args) throws IOException {
+		Inflearn_2_2 inflearn_2_2 = new Inflearn_2_2();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] studentHeights = getStudentHeights(br);
+
+		int visibleStudentCount = inflearn_2_2.getVisibleStudentCount(n, studentHeights);
+		System.out.println(visibleStudentCount);
+	}
+
+	private static int[] getStudentHeights(BufferedReader br) throws IOException {
+		return Arrays.stream(br.readLine()
+				.split(" "))
+			.mapToInt(Integer::parseInt)
+			.toArray();
+	}
+
+	public int getVisibleStudentCount(int n, int[] studentHeights) {
+		int previousStudentHeight = studentHeights[0];
+		int count = 1;
+
+		for (int i = 1; i < n; i++) {
+			if (previousStudentHeight < studentHeights[i]) {
+				previousStudentHeight = studentHeights[i];
+				count++;
+			}
+		}
+
+		return count;
+	}
+}
