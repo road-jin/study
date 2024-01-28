6eb8648a3b9b1781712ce7da43398b6606ac26ef
road-jin <rjswmtk@gmail.com>
2024-01-29 08:36:22 +0900

    feat: 인프런 코테 1문제 학습
    소수 판별(에라토스테네스의 체)

---
+++ condingtest/src/main/java/string/Inflearn_2_5.java
@@ -0,0 +1,33 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Inflearn_2_5 {
+
+	public static void main(String[] args) throws IOException {
+		Inflearn_2_5 inflearn_2_5 = new Inflearn_2_5();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int decimalCount = inflearn_2_5.getDecimalCount(n);
+		System.out.println(decimalCount);
+	}
+
+	public int getDecimalCount(int n) {
+		boolean[] decimals = new boolean[n + 1];
+		int count = 0;
+
+		for (int i = 2; i <= n; i++) {
+			if (!decimals[i]) {
+				count++;
+			}
+
+			for (int j = i + i; j <= n; j += i) {
+				decimals[j] = true;
+			}
+		}
+
+		return count;
+	}
+}
