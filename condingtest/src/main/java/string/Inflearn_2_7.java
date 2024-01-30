053f69924a063aa94b3ac77547c2716238f93c21
road-jin <rjswmtk@gmail.com>
2024-01-31 08:48:39 +0900

    feat: 인프런 코테 1문제 학습
    점수계산(가산점)

---
+++ condingtest/src/main/java/string/Inflearn_2_7.java
@@ -0,0 +1,39 @@
+package string;
+
+import java.util.Scanner;
+
+public class Inflearn_2_7 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int questionCount = in.nextInt();
+		int[] questionResults = getQuestionResults(questionCount, in);
+		System.out.println(soluction(questionResults));
+	}
+
+	public static int[] getQuestionResults(int questionCount, Scanner in) {
+		int[] questionResults = new int[questionCount];
+
+		for (int i = 0; i < questionCount; i++) {
+			questionResults[i] = in.nextInt();
+		}
+
+		return questionResults;
+	}
+
+	public static int soluction(int[] questionResults) {
+		int result = 0;
+		int score = 1;
+
+		for (int questionResult : questionResults) {
+			if (questionResult == 1) {
+				result += score++;
+				continue;
+			}
+
+			score = 1;
+		}
+
+		return result;
+	}
+}
